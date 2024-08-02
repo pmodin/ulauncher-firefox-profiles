1833deef28b80617aa1e158edefea9c20455751d
Kleber Silva <kleber.swf@gmail.com>
2024-08-02 11:26:20 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Ulauncher Firefox Profiles
+
+An [Ulauncher](https://ulauncher.io) extension to open Firefox with an user profile.
+
+> Inspired by [Ulaucher Chrome Profiles Extension](https://github.com/FloydJohn/ulauncher-chrome-profiles)
+
+## Screenshot
+
+![Preview](./images/screenshot.png)
+
+## Install
+
+1. Open Ulauncher preferences
+2. Enter "Extensions" Tab
+3. Click "Add extension" on the left
+4. Paste the url: `https://github.com/kleber-swf/ulauncher-firefox-profiles`
+5. Press "Add"
+
+## Configuration
+
+-  **Firefox Profiles Keyword**: The keyword to activate the extension. Default: `fp`.
+
+-  **Firefox Configuration Folder**: Firefox profiles folder. You can find it inside the _"about:profiles"_ page. Default: `~/snap/firefox/common/.mozilla/firefox/`
+
+-  **Firefox Executable**: The command line to start Firefox. Usually _"firefox"_. Default `firefox`.
+
+---
+
+Preview Theme: [Orchis Dark](https://github.com/kleber-swf/orchis-dark-ulauncher)
---
+++ main.py
@@ -0,0 +1,51 @@
+import os
+import subprocess
+import configparser
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
+
+
+def get_profiles(config_folder):
+    config = configparser.ConfigParser()
+    config.read(os.path.join(config_folder, 'profiles.ini'))
+    return [config[p]['Name'] for p in config.sections() if p != 'General']
+
+
+class DemoExtension(Extension):
+    def __init__(self):
+        super(DemoExtension, self).__init__()
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+        self.subscribe(ItemEnterEvent, ItemEnterEventListener())
+
+
+class KeywordQueryEventListener(EventListener):
+    def on_event(self, event, extension):
+        config_folder = os.path.expanduser(extension.preferences['firefox_folder'])
+        profiles = get_profiles(config_folder)
+
+        query = event.get_argument()
+        if query:
+            query = query.strip().lower()
+            profiles = [p for p in profiles if query in p.lower()]
+
+        entries = []
+        for profile in profiles:
+            entries.append(ExtensionResultItem(
+                icon='images/icon.png',
+                name=profile,
+                on_enter=ExtensionCustomAction(profile, keep_app_open=False)
+            ))
+        return RenderResultListAction(entries)
+
+
+class ItemEnterEventListener(EventListener):
+    def on_event(self, event, extension):
+        subprocess.Popen([extension.preferences['firefox_cmd'], '-p', event.get_data()])
+
+
+if __name__ == '__main__':
+    DemoExtension().run()
